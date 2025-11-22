e8f4aba9547af030b031affd3bca906e406000ad
Patdev2020 <patdev2020@gmail.com>
2025-11-22 15:59:23 -0800

    Initial commit

---
+++ 10a-loadbalancer.tf
@@ -0,0 +1,44 @@
+resource "aws_lb" "web_tier" {
+  name               = "web-lbl"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.weblb.id]
+  subnets = [
+    aws_subnet.public-us-east-1a.id,
+    aws_subnet.public-us-east_1b.id,
+    aws_subnet.public-us-east_1c.id
+
+  ]
+  enable_deletion_protection = false
+  #Lots of death and suffering here, make sure it's false
+
+  tags = {
+    Name    = "vanessaLoadBalancer"
+    Service = "vanessa"
+    Owner   = "User"
+    Project = "Web Service"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.web_tier.arn
+  #   load_balancer_arn = aws_lb.web_tier.arn
+  #   load_balancer_arn = aws_lb.vanessa_alb.arn
+  port     = 80
+  protocol = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.webtier.arn
+    # target_group_arn = aws_lb_target_group.weblb.arn
+    # target_group_arn = aws_lb_target_group.vanessa_tg.arn
+  }
+}
+
+output "lb_dns_name" {
+  value = aws_lb.web_tier.dns_name
+  #   value = aws_lb.web_alb.dns_name
+
+  description = "The DNS name of the web Load Balancer."
+}
---
+++ 10b-scalling-policy.tf
@@ -0,0 +1,15 @@
+resource "aws_autoscaling_policy" "webpoly" {
+  name                   = "web_cpu_scaler"
+  autoscaling_group_name = aws_autoscaling_group.webtier.name
+  #   autoscaling_group_name = aws_autoscaling_group.web_asg.name
+
+  policy_type = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 40.0
+  }
+
+}
